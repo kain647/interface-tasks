9acfd9619f3ed8bc3d9eedba374eeb204b7c5e55
Aleksey <aleksey.amelchenko@gmail.com>
2020-10-28 15:40:27 +0300

    Initialize project using Create React App

--- src/profile/index.js
+++ src/profile/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { GoThreeBars } from "react-icons/go";
 import { RiSearch2Line } from "react-icons/ri";
 import { BiPlus } from "react-icons/bi";
@@ -22,8 +22,8 @@ import {
   TitleTasks,
   SubtitleTasks,
   CheckBox,
-  Line,
-  Box
+  Box,
+  BoxContainer
 } from "./styles";
 
 const Profile = props => {
@@ -33,81 +33,48 @@ const Profile = props => {
     "https://bit.ly/2TDHVKB",
     "https://bit.ly/35xKKlP"
   ];
+
   const tasks = [
     {
+      id: 1,
       time: "24",
       photo: images[0],
       title: "Coffee with Adam",
       subtitle: "Starbucks"
     },
     {
+      id: 2,
       time: "35",
       photo: images[1],
       title: "Lunch with Kate",
       subtitle: "Restaurant"
     },
     {
+      id: 3,
       time: "60",
       photo: images[2],
       title: "Training",
       subtitle: "Boxing hall"
     },
     {
-      time: "60",
-      photo: images[3],
-      title: "Visit doctor",
-      subtitle: "Ophthalmologist"
-    },
-    {
-      time: "24",
-      photo: images[0],
-      title: "Coffee with Adam",
-      subtitle: "Starbucks"
-    },
-    {
-      time: "35",
-      photo: images[1],
-      title: "Lunch with Kate",
-      subtitle: "Restaurant"
-    },
-    {
-      time: "60",
-      photo: images[2],
-      title: "Training",
-      subtitle: "Boxing hall"
-    },
-    {
-      time: "60",
-      photo: images[3],
-      title: "Visit doctor",
-      subtitle: "Ophthalmologist"
-    },
-    {
-      time: "24",
-      photo: images[0],
-      title: "Coffee with Adam",
-      subtitle: "Starbucks"
-    },
-    {
-      time: "35",
-      photo: images[1],
-      title: "Lunch with Kate",
-      subtitle: "Restaurant"
-    },
-    {
-      time: "60",
-      photo: images[2],
-      title: "Training",
-      subtitle: "Boxing hall"
-    },
-    {
-      time: "60",
+      id: 4,
+      time: "20",
       photo: images[3],
       title: "Visit doctor",
       subtitle: "Ophthalmologist"
     }
   ];
+  const [checked, setChecked] = useState({});
 
+  const handleClick = id => {
+    const value = checked[id];
+    const newValue = value === undefined ? true : !value;
+    const newState = {
+      ...checked,
+      [id]: newValue
+    };
+    setChecked(newState);
+  };
   return (
     <Container>
       <ImageContainer>
@@ -123,45 +90,49 @@ const Profile = props => {
           </TitleContainer>
         </HeaderContainer>
         <Box>
-          {/*<WhiteBox />*/}
           <Plus>
             <BiPlus color="#fff" />
           </Plus>
         </Box>
       </ImageContainer>
       {tasks.map(task => {
-        const { time, photo, title, subtitle } = task;
+        const { time, photo, title, subtitle, id } = task;
+        const active = checked[id];
         return (
-          <>
-            <Tasks
-              time={time}
-              photo={photo}
-              title={title}
-              subtitle={subtitle}
-            />
-            <Line />
-          </>
+          <Task
+            key={id}
+            time={time}
+            photo={photo}
+            title={title}
+            subtitle={subtitle}
+            active={active || false}
+            onClick={() => {
+              handleClick(id);
+            }}
+          />
         );
       })}
     </Container>
   );
 };
 
-const Tasks = props => {
-  const { time, photo, title, subtitle } = props;
+const Task = props => {
+  const { time, photo, title, subtitle, active, onClick } = props;
   return (
-    <InfoContainer>
+    <InfoContainer onClick={onClick}>
       <TimeContainer>
         <Time>{time}</Time>
         <Minutes>min</Minutes>
       </TimeContainer>
-      <AvatarTasks photo={photo} />
-      <TasksContainer>
-        <TitleTasks>{title}</TitleTasks>
-        <SubtitleTasks>{subtitle}</SubtitleTasks>
-      </TasksContainer>
+      <BoxContainer>
+        <AvatarTasks photo={photo} />
+        <TasksContainer>
+          <TitleTasks>{title}</TitleTasks>
+          <SubtitleTasks>{subtitle}</SubtitleTasks>
+        </TasksContainer>
+      </BoxContainer>
       <CheckBox>
-        <input type="checkbox" />
+        <input type="checkbox" checked={active} readOnly />
       </CheckBox>
     </InfoContainer>
   );
