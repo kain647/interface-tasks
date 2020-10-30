0db5f9a5731611606728960e45cbf5a0cf5391e3
Aleksey <aleksey.amelchenko@gmail.com>
2020-10-30 14:50:26 +0300

    Initialize project using Create React App

--- src/profile/index.js
+++ src/profile/index.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Clock from "../clock";
 import { GoThreeBars } from "react-icons/go";
 import { RiSearch2Line } from "react-icons/ri";
 import { BiPlus } from "react-icons/bi";
@@ -31,7 +32,8 @@ const Profile = props => {
     "https://bit.ly/2Hwyl9o",
     "http://bit.ly/2YuJwn7",
     "https://bit.ly/2TDHVKB",
-    "https://bit.ly/35xKKlP"
+    "https://bit.ly/35xKKlP",
+    "https://bit.ly/3kIumFp"
   ];
 
   const tasks = [
@@ -62,8 +64,51 @@ const Profile = props => {
       photo: images[3],
       title: "Visit doctor",
       subtitle: "Ophthalmologist"
+    },
+    {
+      id: 5,
+      time: "120",
+      photo: images[4],
+      title: "Meeting a friend",
+      subtitle: "Bar friends"
+    },
+    {
+      id: 6,
+      time: "24",
+      photo: images[0],
+      title: "Coffee with Adam",
+      subtitle: "Starbucks"
+    },
+    {
+      id: 7,
+      time: "35",
+      photo: images[1],
+      title: "Lunch with Kate",
+      subtitle: "Restaurant"
+    },
+    {
+      id: 8,
+      time: "60",
+      photo: images[2],
+      title: "Training",
+      subtitle: "Boxing hall"
+    },
+    {
+      id: 9,
+      time: "20",
+      photo: images[3],
+      title: "Visit doctor",
+      subtitle: "Ophthalmologist"
+    },
+    {
+      id: 10,
+      time: "120",
+      photo: images[4],
+      title: "Meeting a friend",
+      subtitle: "Bar friends"
     }
   ];
+
   const [checked, setChecked] = useState({});
 
   const handleClick = id => {
@@ -75,23 +120,35 @@ const Profile = props => {
     };
     setChecked(newState);
   };
+
+  const taskNumber = tasks.length;
+  const activeNumber = tasks.filter(task => checked[task.id]).length;
+  const activeTask = taskNumber - activeNumber;
+
+  //const textTask = activeTask > 1 ? "Tasks" : "Task";
+  const textTask = `Task${activeTask > 1 ? "s" : ""}`
+
   return (
     <Container>
       <ImageContainer>
         <HeaderContainer>
           <Header>
-            <GoThreeBars cursor="pointer" />
-            <RiSearch2Line cursor="pointer" />
+            <GoThreeBars />
+            <RiSearch2Line />
           </Header>
           <Avatar />
           <TitleContainer>
-            <Title>6 Tasks</Title>
-            <Subtitle>april 7, 2020</Subtitle>
+            <Title>
+              {activeTask} {textTask}
+            </Title>
+            <Subtitle>
+              <Clock />
+            </Subtitle>
           </TitleContainer>
         </HeaderContainer>
         <Box>
           <Plus>
-            <BiPlus color="#fff" />
+            <BiPlus />
           </Plus>
         </Box>
       </ImageContainer>
@@ -119,7 +176,7 @@ const Profile = props => {
 const Task = props => {
   const { time, photo, title, subtitle, active, onClick } = props;
   return (
-    <InfoContainer onClick={onClick}>
+    <InfoContainer onClick={onClick} active={active}>
       <TimeContainer>
         <Time>{time}</Time>
         <Minutes>min</Minutes>
